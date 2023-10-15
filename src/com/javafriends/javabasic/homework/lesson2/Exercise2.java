69390a53f0f0eaa6a4c88cd6c7f590a85ef96d8c
Bondarenko Anatolii <bondkrkut@gmail.com>
2023-10-15 22:06:45 +0300

    Exercise2 is done.

---
+++ src/com/javafriends/javabasic/homework/lesson2/Exercise2.java
@@ -0,0 +1,11 @@
+package com.javafriends.javabasic.homework.lesson2;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        String latitude = "48° 9′ 36″ N";
+        String longitude = "24° 30′ 1″ E";
+
+        System.out.println("Mount Hoverla is located at the coordinates - " + latitude + ", " + longitude);
+
+    }
+}
