eec4785db7b35c58dde64976f6ce0e881ab1e935
Bondarenko Anatolii <bondkrkut@gmail.com>
2023-11-05 17:35:13 +0200

    Corrected - both small and large letters should be present

--- src/com/javafriends/javabasic/homework/lesson11/Test.java
+++ src/com/javafriends/javabasic/homework/lesson11/Test.java
@@ -39,8 +39,6 @@ public class Test {
             System.out.println(passwordGenerator2.nextPassword());
             System.out.println();
 
-            PasswordGenerator passwordGenerator3 = new PasswordGenerator(2);
-            System.out.println(passwordGenerator3.nextPassword());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
